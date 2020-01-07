e9f6032fbe9f70943cb102080c994037c45b239f
Hendi Santika <hendisantika@gmail.com>
2020-01-08 06:08:16 +0700

    #7 - Add override execute in CheckBalanceAmountDelegate

--- src/main/java/com/hendisantika/springbootcamunda/delegate/CheckBalanceAmountDelegate.java
+++ src/main/java/com/hendisantika/springbootcamunda/delegate/CheckBalanceAmountDelegate.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootcamunda.delegate;
 
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,5 +19,29 @@ import org.springframework.context.annotation.Description;
 public class CheckBalanceAmountDelegate implements JavaDelegate {
 
     private Logger logger = LoggerFactory.getLogger(CheckBalanceAmountDelegate.class);
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception
+    {
+        // current balance on account...
+        Double currentBalanceAmount = 1500.00;
+
+        // get desired amount
+        Double desiredAmount = Double.valueOf(delegateExecution.getVariable("amount").toString());
+        logger.info("Desired amount: {}", desiredAmount);
+
+        // check if there is enough funds...
+        if (desiredAmount <= currentBalanceAmount)
+        {
+            delegateExecution.setVariable("enough_funds", true);
+            logger.info("There is enough funds on account.");
+            logger.info("You have left: {} funds.", currentBalanceAmount - desiredAmount);
+        }
+        else
+        {
+            logger.warn("There is not enough funds on account.");
+            delegateExecution.setVariable("enough_funds", false);
+        }
+    }
 
 }
