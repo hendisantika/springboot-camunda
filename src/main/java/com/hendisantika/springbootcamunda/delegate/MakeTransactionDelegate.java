08330f88db98d148791a198b213b5bdbb112ade7
Hendi Santika <hendisantika@gmail.com>
2020-01-08 06:10:37 +0700

    #12 - Add MakeTransactionDelegate

---
+++ src/main/java/com/hendisantika/springbootcamunda/delegate/MakeTransactionDelegate.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootcamunda.delegate;
+
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Description;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-camunda
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/01/20
+ * Time: 06.10
+ */
+@Description(value = "Delegate responsible for making transaction.")
+public class MakeTransactionDelegate implements JavaDelegate {
+
+    private final Logger logger = LoggerFactory.getLogger(MakeTransactionDelegate.class);
+}
