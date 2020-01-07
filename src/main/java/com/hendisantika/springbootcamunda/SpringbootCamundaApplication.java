d3a674b86f8e58986a3fffd8b31d2975a1babffb
Hendi Santika <hendisantika@gmail.com>
2020-01-08 05:54:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootcamunda/SpringbootCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcamunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCamundaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootcamunda/SpringbootCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcamunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCamundaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
