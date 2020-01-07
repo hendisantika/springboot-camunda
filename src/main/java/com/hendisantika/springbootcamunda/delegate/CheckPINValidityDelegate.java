6bdb0e5ab3314944302786afc99f1c255daa6084
Hendi Santika <hendisantika@gmail.com>
2020-01-08 06:09:14 +0700

    #9 - Add execute in CheckPINValidityDelegate

--- src/main/java/com/hendisantika/springbootcamunda/delegate/CheckPINValidityDelegate.java
+++ src/main/java/com/hendisantika/springbootcamunda/delegate/CheckPINValidityDelegate.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootcamunda.delegate;
 
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,4 +18,18 @@ import org.springframework.context.annotation.Description;
 @Description(value = "Delegate for validating PIN.")
 public class CheckPINValidityDelegate implements JavaDelegate {
     private final Logger logger = LoggerFactory.getLogger(CheckPINValidityDelegate.class);
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        Long pinCode = (Long) delegateExecution.getVariable("pin_code");
+        if (pinCode == null) {
+            logger.error("PIN code is not provided");
+            throw new Exception("Pin code is not provided!");
+        }
+
+        logger.info("User has entered the following PIN code: {}", pinCode);
+
+        // set valid PIN variable.
+        delegateExecution.setVariable("is_valid_pin", true);
+    }
 }
